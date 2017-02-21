d24af25c586bb0fa711e214f104c3689c1c069b8
Emil Blomquist <mr.emil.blomquist@gmail.com>
2017-02-21 13:48:59 +0100

    writing to file instead of writing to terminal

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,38 +1,73 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include <iomanip> // setprecision
+#include <sstream> // stringstream
+
 #include "DMC/DiagrammaticMonteCarlo.h"
 
-int main() {
+using namespace std;
 
+int main () {
+
+  // parameters
+  double
+    p = 2,
+    alpha = 2,
+    mu = -2.2;
+
+  // num iterations for each data point
+  int N = 10000000;
+
+  // external momentum
+  Vector3d externalMomentum(p, 0, 0);
+
+  // create vector with time data points
   vector<double> times(250, 0);
   for(int i = 0; i != times.size(); i++) {
     times[i] = 0.01 + i*5.0/(times.size() - 1);
   }
 
-  Vector3d externalMomentum(1, 0, 0);
-  double 
-    alpha = 2,
-    mu = -2.2;
+  // create date string to be written to file
+  time_t rawtime;
+  struct tm * timeinfo;
+  char buffer[80];
+  time (&rawtime);
+  timeinfo = localtime(&rawtime);
+  strftime(buffer, 80, "%Y-%m-%d %I:%M:%S", timeinfo);
+  string dateString(buffer);
 
-  int N = 10000000;
+  // create file name
+  stringstream stream;
+  stream << fixed << setprecision(3)
+         << "p="<< p << " a=" << alpha << " mu=" << mu;
+  string fileName = stream.str();
 
+  // write to file
+  ofstream myfile;
+  myfile.open("../data/" + fileName + ".txt", ios_base::app);
+  myfile << "-------- " + dateString + " --------" << endl;
+  myfile.close();
 
-  for(int i = 79; i != times.size(); i++) {
-
+  // for each of our time data points
+  for(int i = 0; i != times.size(); i++) {
+    // initiate DMC
     DiagrammaticMonteCarlo DMC(externalMomentum, times[i], alpha, mu);
 
-
+    // obtain g0 before we update the diagram
     double g0 = DMC.FD();
 
+    // run the algorithm
     auto orderWeights = DMC.run(N, 7);
 
-    // precision and shit
-    cout << std::setprecision(7);
-    cout << fixed;
-
-    cout << times[i] << " ";
+    // data to write to file
+    myfile.open("../data/" + fileName + ".txt", ios_base::app);
+    myfile << fixed << setprecision(7) << times[i] << " ";
     for (auto order : orderWeights) {
-      cout << g0*order/orderWeights[0] <<  " ";
+      myfile << fixed << setprecision(7) << g0*order/orderWeights[0] <<  " ";
     }
-    cout << g0/orderWeights[0] << endl;
+    myfile << fixed << setprecision(7) << g0/orderWeights[0] << endl;
+    myfile.close();
   }
 }
