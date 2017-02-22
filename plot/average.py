425f3091711cc252a18c48493d1a01a14fb87385
Emil Blomquist <mr.emil.blomquist@gmail.com>
2017-02-22 17:41:52 +0100

    temp save

---
+++ plot/average.py
@@ -0,0 +1,74 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+file1 = 'p=0.000 a=2.000 mu=-2.200 N=10000000 time=2017-02-22 01:09:51'
+file2 = 'data-0'
+
+
+T1 = [];
+G1 = [];
+with open('data/' + file1 + '.txt') as fp:
+  for i, line in enumerate(fp):
+
+    # skip first line
+    if i == 0:
+      print(line[0:-1])
+      continue
+
+    l = line.split('\n')[0]
+    l = l.split(' ')
+
+    if i == 1:
+      Gs1 = np.array(l[1:-1])
+    else:
+      Gs1 = np.vstack([Gs1, l[1:-1]])
+
+    T1.append(l[0])
+    G1.append(l[-1])
+
+
+T2 = [];
+G2 = [];
+with open('data/' + file2 + '.txt') as fp:
+  for i, line in enumerate(fp):
+
+    # skip first line
+    if i == 0:
+      continue
+
+    l = line.split('\n')[0]
+    l = l.split(' ')
+
+    if i == 1:
+      Gs2 = np.array(l[1:-1])
+    else:
+      Gs2 = np.vstack([Gs2, l[1:-1]])
+
+    T2.append(l[0])
+    G2.append(l[-1])
+
+
+
+
+
+if len(T1) != len(T2):
+  print('Not the same amount of data points')
+else:
+
+  for i in range(0, len(T1)):
+    dt = abs(float(T1[i]) - float(T2[i]))
+    if dt > 10**-5:
+      print('ERROR: times do not match at row ', i, ' which corresponds to t≈', T1[i])
+    else:
+      string = "{:.7f}".format(0.5*(float(T1[i]) + float(T2[i]))) + ' '
+      for j in range(0, Gs1.shape[1]):
+        string += "{:.7f}".format(0.5*(float(Gs1[i, j]) + float(Gs2[i, j]))) + ' '
+      string += "{:.7f}".format(0.5*(float(G1[i]) + float(G2[i])))
+
+    print(string)
+
+# plot G
+plt.plot(T1, G1, lw=2, color="brown", label=r'$\sum G^i$')
+plt.plot(T2, G2, lw=2, color="red", label=r'$\sum G^i$')
+plt.show()
--- plot/main.py
+++ plot/main.py
@@ -1,32 +1,36 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+fileName = 'p=0.000 a=2.000 mu=-2.200 N=20000000 time=2017-02-22 01:09:51'
+fileName = 'p=1.000 a=2.000 mu=-2.200 N=10000000 time=2017-02-?? ??:??:??'
+fileName = 'p=1.667 a=2.000 mu=-2.200 N=10000000 time=2017-02-22 01:09:51'
+fileName = 'p=3.333 a=2.000 mu=-2.200 N=10000000 time=2017-02-22 01:09:51'
+fileName = 'p=5.000 a=2.000 mu=-2.200 N=10000000 time=2017-02-22 01:09:51'
+
 T = [];
 G = [];
 
-if False:
-  file = "data-0-0-0.txt"
-  P = "(0, 0, 0)"
-else:
-  file = "data-1-0-0.txt"
-  P = "(1, 0, 0)"
-
-
-with open(file) as fp:
+with open('data/' + fileName + '.txt') as fp:
   for i, line in enumerate(fp):
 
-    l = line.split('\n')[0]
-    l = l.split(' ')
-
+    # skip first line
     if i == 0:
-      Gs = np.array(l[1:-1])
+      print(line[0: -1])
     else:
-      Gs = np.vstack([Gs, l[1:-1]])
 
-    # print(l)
+      l = line.split('\n')[0]
+      l = l.split(' ')
 
-    T.append(l[0])
-    G.append(l[-1])
+      if i == 1:
+        config = l
+      else:
+        if i == 2:
+          Gs = np.array(l[1:-1])
+        else:
+          Gs = np.vstack([Gs, l[1:-1]])
+
+        T.append(l[0])
+        G.append(l[-1])
 
 for i, val in enumerate(Gs[0, :]):
   plt.plot(T, Gs[:, i], label=r'$G^' +  str(i) + '$')
@@ -35,11 +39,10 @@ plt.plot(T, G, lw=2, color="brown", label=r'$\sum G^i$')
 
 plt.xlim(0, 5)
 plt.xlabel(r'$\tau$', fontsize=20)
+plt.ylabel(config[4] + ' ' + config[5])
 
-titleText = '$\mathbf{p} = ' + P + ', \, \alpha = 2, \, \mu = -2.2$'
-
-plt.title(r'$\mathbf{{p}} = {0}, \, \alpha = 2, \, \mu = -2.2$'.format(P))
+plt.title(r'$p = {0}, \, \alpha = {1}, \, \mu = {2}, \, N = {3}$'.format(config[0], config[1], config[2], config[3]))
 plt.legend(loc=1)
 
-plt.savefig('plot.pdf')
+plt.savefig('plots/' + fileName + '.pdf')
 plt.show()
